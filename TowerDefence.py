eb3069d8ca6b744c3e521275121ce54d2cb8cf22
Haku026 <aton452878@gmail.com>
2025-07-11 01:58:17 +0900

    modified:   TowerDefence.py 	new file:   "\352\262\214\354\236\204\354\242\205\353\243\214\355\231\224\353\251\264.png" 	new file:   "\352\270\260\353\252\250\354\260\214.png" 	new file:   "\353\251\224\354\235\270\353\251\224\353\211\264 UI.png" 	new file:   "\353\260\260\352\262\275\354\235\214\354\225\205/\352\262\214\354\236\204\354\235\214\354\225\205.mp3" 	new file:   "\353\260\260\352\262\275\354\235\214\354\225\205/\353\247\265\354\204\240\355\203\235bgm.mp3" 	new file:   "\353\260\260\352\262\275\354\235\214\354\225\205/\353\260\260\352\262\275\354\235\214\354\225\205.mp3" 	new file:   "\353\260\260\352\262\275\354\235\214\354\225\205/\354\213\234\354\236\221 \355\232\250\352\263\274\354\235\214.wav" 	new file:   "\353\263\264\355\206\265\353\247\265.png" 	new file:   "\354\211\254\354\233\200\353\247\265.png" 	new file:   "\354\226\264\353\240\244\354\233\200\353\247\265.png"

--- TowerDefence.py
+++ TowerDefence.py
@@ -3,7 +3,6 @@ import pygame
 pygame.init()
 
 muhyun = 1
-#r김후후
 # 화면 생성
 GameDisplay = pygame.display.set_mode((1920, 1080))
 pygame.display.set_caption("타워 디펜스")
@@ -11,8 +10,21 @@ pygame.display.set_caption("타워 디펜스")
 # 이미지 불러오기
 background1 = pygame.image.load("./대충 개쩌는 배경.png")
 background2 = pygame.image.load("./시작화면.png")
-gameExitMes = pygame.image.load("./gameExitMes.png")
+gameExitMes = pygame.image.load("./게임종료화면.png")
+easyMapUI = pygame.image.load("./쉬움맵.png")
+normalMapUI = pygame.image.load("./보통맵.png")
+hardMapUI = pygame.image.load("./어려움맵.png")
+mainMenuUI = pygame.image.load("./메인메뉴 UI.png")
 
+# 음악 불러오기
+mainMenuMusic = pygame.mixer.Sound("./배경음악/배경음악.mp3")
+mainMenuMusic.set_volume(0.4)  # 볼륨 설정 (0.0 ~ 1.0)
+
+enterSoundEffect = pygame.mixer.Sound("./배경음악/시작 효과음.wav")
+enterSoundEffect.set_volume(1.0)
+
+mapSelectBGM = pygame.mixer.Sound("./배경음악/맵선택bgm.mp3")
+mapSelectBGM.set_volume(0.4)
 
 # 폰트 설정
 font = pygame.font.Font("./DungGeunMo.ttf", 80)
@@ -23,11 +35,15 @@ exitMes = font.render("게임을 종료하시겠습니까?", True, (0, 0, 0))
 exitMesTrue = font.render("네", True, (0, 0, 0))
 exitMesFalse = font.render("아니오", True, (0, 0, 0))
 
-
 # 상태 변수들
 gamepage = 0
 running = True
 esc_mode = False
+
+# 음악 관련 상태 변수들
+isMainMusicOn = True
+isMainMusicEffectOn = False
+isMapSelectMusicOn = False
 
 # 게임 루프
 while running:
@@ -35,20 +51,40 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
+        # 마우스 클릭 위치 출력
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            print(event.pos)
+
         # 시작 화면에서 엔터키 입력
         if gamepage == 0:
             if event.type == pygame.KEYDOWN and event.key == pygame.K_RETURN:
+                isMainMusicEffectOn = True
+                mainMenuMusic.stop()
+                isMapSelectMusicOn = True
                 gamepage = 1
+                
+        # 음악 발생 처리
+        if isMainMusicEffectOn == True:
+            enterSoundEffect.play()
+            isMainMusicEffectOn = False
+
+        if isMainMusicOn == True:
+            mainMenuMusic.play(-1)
+            isMainMusicOn = False
+
+        if isMapSelectMusicOn == True:
+            mapSelectBGM.play(-1)
+            isMapSelectMusicOn = False
 
 
-        #ESC키를 누르면 종료
+        # ESC키를 누르면 종료
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_ESCAPE:
                 esc_mode = not esc_mode
+        # 게임 종료처리 or 남기처리
         if esc_mode:
             yesButton = pygame.Rect(533, 659, 321, 128)
             noButton = pygame.Rect(1047, 650, 354, 126)
-            #게임 종료처리 or 남기처리
             if event.type == pygame.MOUSEBUTTONDOWN:
                 if event.button == 1:
                     if yesButton.collidepoint(pygame.mouse.get_pos()):
@@ -58,23 +94,24 @@ while running:
                         esc_mode = False
 
 
+
     # 화면 그리기
+    # 메인화면
     if gamepage == 0:
         GameDisplay.blit(background1, (0, 0))
         GameDisplay.blit(start, (500, 490))
-
+    # 메인화면
     elif gamepage == 1:
         GameDisplay.blit(background2, (0, 0))
+        GameDisplay.blit(mainMenuUI, (150,780))
+    
 
 
-        if esc_mode:
+    if esc_mode:
             GameDisplay.blit(gameExitMes, (360,203))
             GameDisplay.blit(exitMes, (425,350))
             GameDisplay.blit(exitMesTrue, (660, 670))
-            GameDisplay.blit(exitMesFalse, (1100, 670))
-
-
-
+            GameDisplay.blit(exitMesFalse, (1100, 670))    
 
     pygame.display.update()
 
